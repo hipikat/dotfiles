c3006069b2c0e0da1abb5fae13efb625f2fbcd27
Adam Wright <adam@hipikat.org>
2018-05-08 08:50:07 +0000

    Rage-quit attempts at headless VundleInstall

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 python3 plumb_files.py --current-user --force
 git clone https://github.com/VundleVim/Vundle.vim.git ../.vim/bundle/Vundle.vim
-vim - -nes -c ':VundleInstall' -c ':qa!'
+
+# Having just a BIT of trouble getting this to process with Salt...
+#vim - -nes -c ':VundleInstall' -c ':qa!'
