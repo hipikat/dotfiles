a37ac73741ee5151b5d353a00fbe0fc510b7a249
Adam Wright <adam@hipikat.org>
2018-05-08 08:05:01 +0000

    More attemps to install Vim properly sans terminal

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python3 plumb_files.py --current-user --force
 git clone https://github.com/VundleVim/Vundle.vim.git ../.vim/bundle/Vundle.vim
-ex -c VundleInstall -c qa
+vim -E -s -c VundleInstall -c qa
