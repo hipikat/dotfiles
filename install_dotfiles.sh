52f2c13727e33ccffd76f323d8583fb5f93d5fee
Adam Wright <adam@hipikat.org>
2018-05-08 08:21:39 +0000

    Fuck it

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python3 plumb_files.py --current-user --force
 git clone https://github.com/VundleVim/Vundle.vim.git ../.vim/bundle/Vundle.vim
-vim -E -s -c VundleInstall -c qa
+# fuck it
+#vim -E -s -c VundleInstall -c qa
