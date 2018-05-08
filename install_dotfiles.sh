d17f231dfd90e8a2c4c35049c1c360742bfe118d
Adam Wright <adam@hipikat.org>
2018-05-08 08:37:23 +0000

    One (or two) more tries for install_dotfiles.sh

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 python3 plumb_files.py --current-user --force
 git clone https://github.com/VundleVim/Vundle.vim.git ../.vim/bundle/Vundle.vim
-# fuck it
-#vim -E -s -c VundleInstall -c qa
+vim - -nes -c ':VundleInstall' -c ':qa!'
