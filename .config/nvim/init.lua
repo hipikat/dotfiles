3b4e261ef9517994c99ad8f65b1679f72d384929
Ada Wright <ada@hpk.io>
2024-08-03 10:42:34 +0000

    Remove broken Neovim Packer setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,40 @@
+-- Plugins
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- add your plugins here
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+
+--------
 -- Set default options
 vim.opt.autoindent = true -- Auto-indenting
 vim.opt.hlsearch = false  -- No highlighting search matches
@@ -79,13 +116,11 @@ function TogglePasteMode()
     end
 end
 
-
 -- Initialize paste mode status
 vim.g.paste_mode_enabled = false
 
 -- Set keymap to toggle paste mode and restore tab settings
 vim.api.nvim_set_keymap('n', '<leader>v', ':lua TogglePasteMode()<CR>', { noremap = true, silent = true })
-
 
 -- Map F-keys to tabs for normal and insert modes
 local function set_tab_keymap(key, tab_number)
@@ -102,3 +137,4 @@ end
 for i, char in ipairs({'#', '*', '=', '-', '^'}) do
     vim.api.nvim_set_keymap('n', '<leader>'..i, 'yyPVr'..char..'yyjp', { noremap = true, silent = true })
 end
+
